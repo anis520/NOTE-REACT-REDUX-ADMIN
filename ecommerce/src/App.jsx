1b1e074a2d2caae2484ac954bae20c692340f82d
anis520 <anisulhoque587@gmail.com>
2023-08-20 11:17:48 +0600

    version2.1

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routers/publicRoutes";
 import "./assets/vendor/fontawesome-free/css/all.min.css";
+import "./assets/vendor/animate/animate.min.css";
+import "./assets/vendor/photoswipe/photoswipe.min.css";
+import "./assets/vendor/photoswipe/default-skin/default-skin.min.css";
+import "./assets/vendor/magnific-popup/magnific-popup.min.css";
+
+import "./assets/vendor/swiper/swiper-bundle.min.css";
+import "./assets/vendor/nouislider/nouislider.min.css";
+
+import "./assets/css/demo1.min.css";
+import "./assets/css/style.min.css";
+import Layouts from "./layouts/Layouts";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <RouterProvider router={router}></RouterProvider>;
+  return (
+    <>
+      <RouterProvider router={router}></RouterProvider>;
+    </>
+  );
 }
 
 export default App;
